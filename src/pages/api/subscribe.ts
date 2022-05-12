a5e227302c9f5db0b06a1af13bc1fc352c366795
Manuel Molina <manuelanmolina@gmail.com>
2022-05-12 11:10:27 -0300

    atualizando aplicacao | adicionando webhooks stripe | implementando funcionalidades de inscricao de usuario

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,18 +1,47 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { stripe } from "../../service/stripe";
 import { getSession } from "next-auth/react";
+import { fauna } from "../../service/faunadb";
+import { query as q } from "faunadb";
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const session = await getSession({ req });
 
-    const stripeCostumer = await stripe.customers.create({
-      email: session.user.email,
-    });
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
+
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeCostumer = await stripe.customers.create({
+        email: session.user.email,
+      });
+
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCostumer.id,
+          },
+        })
+      );
+
+      customerId = stripeCostumer.id;
+    }
 
     const checkoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCostumer.id,
+      customer: customerId,
       payment_method_types: ["card"],
       billing_address_collection: "required",
       line_items: [{ price: "price_1KLp5jDOPilXXajRqYY5HOX0", quantity: 1 }],
