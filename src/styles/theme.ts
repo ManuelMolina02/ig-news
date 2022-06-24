03e48b1162165a442e58aeaca2a1635c8ea03f9c
Manuel Molina <manuelanmolina@gmail.com>
2022-06-24 20:25:52 -0300

    atualizando theme | inserindo opcoes de imagens | atualizando redme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,9 +5,9 @@ export const themes = {
       bgPrimary: "#121214",
       bgSecondary: "#1f2729",
       color: "#e5e5e5",
+      bgHover: "#ffffff12",
 
       constrastColor: "#ffffffcb",
-
       shadow: "0px 0px 10px 2px #5050504f",
     },
     {
@@ -16,20 +16,50 @@ export const themes = {
       bgSecondary: "#c6d0d3",
       color: "#1f2729",
 
+      bgHover: "#c5c5c57a",
+
       constrastColor: "#ffffffcb",
       shadow: "0px 0px 10px 2px #5050504f",
     },
   ],
   colors: [
     {
-      name: "analogous",
-      primary: "#c40d6f",
+      name: "complementary",
+      primary: "#eba417",
       img: "/images/girl-coding2.svg",
     },
     {
-      name: "complementary",
-      primary: "#eba417",
-      img: "/images/girl-coding.svg",
+      name: "analogous",
+      primary: "#c40d6f",
+      img: "/images/girl-coding1.svg",
+    },
+  ],
+  images: [
+    {
+      name: "girl-coding-1",
+      styles: [
+        {
+          name: "analogous",
+          url: "/images/girl-analogous-1.svg",
+        },
+        {
+          name: "complementary",
+          url: "/images/girl-complementary-1.svg",
+        },
+      ],
+    },
+    {
+      name: "girl-coding-2",
+      styles: [
+        {
+          name: "analogous",
+          url: "/images/girl-analogous-2.svg",
+        },
+        {
+          name: "complementary",
+          url: "/images/girl-complementary-2.svg",
+        },
+      ],
     },
   ],
 };
