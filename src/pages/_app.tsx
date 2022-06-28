1fb85d81268b81ee42f4642e516c42edab965074
Manuel Molina <manuelanmolina@gmail.com>
2022-06-28 19:45:13 -0300

    ajustando ordenacao de importacoes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import { AppProps } from 'next/app'
 import { Header } from '../components/Header';
-import { SessionProvider as NextAuthProvider } from 'next-auth/react';
-import '../styles/globals.scss';
 import { DefineThemeProvider } from '../contexts/theme';
+import { SessionProvider as NextAuthProvider } from 'next-auth/react';
+
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
