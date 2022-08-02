b5aba0fece678d06b111dae14a45b66eafb761f0
Manuel Molina <manuelanmolina@gmail.com>
2022-08-01 21:10:05 -0300

    adicionando description no meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,8 +6,9 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+          <meta name="description" content="News about the React world. Get access to all the publications for $9.90 month." />
         </Head>
         <body>
           <Main />
